ff68db8874374baa9e755bfa8153c69e0892c0f1
1chooo <hugo970217@gmail.com>
2023-08-12 18:48:26 +0800

    restructure python code

--- config_example.py
+++ config_example.py
@@ -9,14 +9,14 @@ Version: v0.0.1
 line_bot_api = 'your_line_bot_api'
 handler = 'your_handler'
 
-# AWS要知道大家是誰，需要類似身份帳號密碼的亂數
+# AWS info
 client_aws_access_key_id = "your_client_aws_access_key_id"
 client_aws_secret_access_key = "your_client_aws_secret_access_key"
 client_aws_session_token="your_client_aws_session_token"
 
-# 模型在AWS的位置
+# Model in AWS
 model_arn="your_model_arn"
 
-# 存放消費者上傳照片的桶子名
+# AWS bucket info
 client_bucket_name="your_client_bucket_name"
 client_region_name="your_client_region_name"
